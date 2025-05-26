bb9e02436c615e6d1ac2092b92e4d06d40937a04
mary-msh <59991552+mary-msh@users.noreply.github.com>
2025-05-25 22:26:35 -0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,1 +1,10 @@
-
+```cpp
+function findDualSportPlayers(basketball_players, football_players) {
+  const basketballSet = new Set(
+    basketball_players.map(p => `${p.first_name} ${p.last_name}`)
+  );
+  return football_players
+    .map(p => `${p.first_name} ${p.last_name}`)
+    .filter(name => basketballSet.has(name));
+}
+```
