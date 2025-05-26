c2cfdd4ad038adcd97e0c6ef33b0bd3adc12fdef
mary-msh <59991552+mary-msh@users.noreply.github.com>
2025-05-25 22:32:05 -0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,10 +1,28 @@
 ```cpp
-function findDualSportPlayers(basketball_players, football_players) {
-  const basketballSet = new Set(
-    basketball_players.map(p => `${p.first_name} ${p.last_name}`)
-  );
-  return football_players
-    .map(p => `${p.first_name} ${p.last_name}`)
-    .filter(name => basketballSet.has(name));
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+using namespace std;
+
+struct Player {
+    string first_name;
+    string last_name;
+};
+
+vector<Player> findDualSportPlayers(vector<Player> basketball_players, vector<Player> football_players) {
+    set<string> basketballSet;
+    for (const auto& p : basketball_players) {
+        basketballSet.insert(p.first_name + " " + p.last_name);
+    }
+
+    vector<Player> result;
+    for (const auto& p : football_players) {
+        string fullName = p.first_name + " " + p.last_name;
+        if (basketballSet.find(fullName) != basketballSet.end()) {
+            result.push_back(p);
+        }
+    }
+    return result;
 }
 ```
